eace1150cc72f0090227113b0d042302b4f2d816
scotfeel <1700229072@qq.com>
2022-07-20 13:32:18 +0800

    给interactionNotice表增加了朋友备注字段

--- src/server/sql/interactionNotice.ts
+++ src/server/sql/interactionNotice.ts
@@ -10,9 +10,9 @@ export function createInteractionNoticeTable(config = { name: 'scotfeel', path: 
     name: 'scotfeel',
     sql:
       'create table if not exists interactionNotice( "id" INTEGER PRIMARY KEY AUTOINCREMENT,"momentId" BIGINT,"momentType" INT(11),"spaceId" VARCHAR(40),"likerId" VARCHAR(40),"likerNickname" VARCHAR(40),' +
-      '"likerAvatar" VARCHAR(40),"spaceLikerRemarkName" VARCHAR(40),"content" VARCHAR(1000),"secondCommentIndex" VARCHAR(40),"commentedUserId" VARCHAR(40),"commentedUserNickname" VARCHAR(40),"commentedUserAvatar" VARCHAR(80),'+
-      '"spaceCommentedUserRemarkName" VARCHAR(80),"commenterId" VARCHAR(80),"commenterNickname" VARCHAR(80),"commenterAvatar" VARCHAR(80),"spaceCommenterRemarkName" VARCHAR(80),"commentType" INT(11),'+
-      '"reposterId" VARCHAR(80),"reposterNickname" VARCHAR(80),"reposterAvatar" VARCHAR(80),"spaceUserRemarkName" VARCHAR(80),"userMomentContent" VARCHAR(2000),"userMomentPhoto" VARCHAR(80),"spaceMomentContent" VARCHAR(2000),'+
+      '"likerAvatar" VARCHAR(80),"likerFriendRemrkName" VARCHAR(40),"spaceLikerRemarkName" VARCHAR(40),"content" VARCHAR(1000),"secondCommentIndex" VARCHAR(40),"commentedUserId" VARCHAR(40),"commentedUserNickname" VARCHAR(40),"commentedUserAvatar" VARCHAR(80),'+
+      '"commentedUserFriendRemarkName" VARCHAR(80),"spaceCommentedUserRemarkName" VARCHAR(80),"commenterId" VARCHAR(80),"commenterNickname" VARCHAR(80),"commenterAvatar" VARCHAR(80),"commenterFriendRemarkName" VARCHAR(80),"spaceCommenterRemarkName" VARCHAR(80),"commentType" INT(11),'+
+      '"reposterId" VARCHAR(80),"reposterNickname" VARCHAR(80),"reposterAvatar" VARCHAR(80),"reposterFriendRemarkName" VARCHAR(80),"spaceUserRemarkName" VARCHAR(80),"userMomentContent" VARCHAR(2000),"userMomentPhoto" VARCHAR(80),"spaceMomentContent" VARCHAR(2000),'+
       '"spaceMomentPhoto" VARCHAR(80),"noticeType" INT(11),"createTime" BIGINT,"belongToId" VARCHAR(40))',
     success() {
       console.log('互动通知表初始化成功');
@@ -31,21 +31,25 @@ export function insertInteractionNotice(
   likerId: string,
   likerNickname: string,
   likerAvatar: string,
+  likerFriendRemrkName: string,
   spaceLikerRemarkName: string,
   content: string,
   secondCommentIndex: string,
   commentedUserId: string,
   commentedUserNickname: string,
   commentedUserAvatar: string,
+  commentedUserFriendRemarkName: string,
   spaceCommentedUserRemarkName: string,
   commenterId: string,
   commenterNickname: string,
   commenterAvatar: string,
+  commenterFriendRemarkName: string,
   spaceCommenterRemarkName: string,
   commentType: number,
   reposterId: string,
   reposterNickname: string,
   reposterAvatar: string,
+  reposterFriendRemarkName: string,
   spaceUserRemarkName: string,
   userMomentContent: string,
   userMomentPhoto: string,
@@ -56,9 +60,9 @@ export function insertInteractionNotice(
   belongToId: string
 ): void {
   return executeSql(`
-		insert into interactionNotice values (null,"${momentId}","${momentType}","${spaceId}","${likerId}","${likerNickname}","${likerAvatar}","${spaceLikerRemarkName}","${content}","${secondCommentIndex}","${commentedUserId}",
-    "${commentedUserNickname}","${commentedUserAvatar}","${spaceCommentedUserRemarkName}","${commenterId}","${commenterNickname}","${commenterAvatar}","${spaceCommenterRemarkName}","${commentType}","${reposterId}",
-    "${reposterNickname}","${reposterAvatar}","${spaceUserRemarkName}","${userMomentContent}","${userMomentPhoto}","${spaceMomentContent}","${spaceMomentPhoto}","${noticeType}","${createTime}","${belongToId}")
+		insert into interactionNotice values (null,"${momentId}","${momentType}","${spaceId}","${likerId}","${likerNickname}","${likerAvatar}","${likerFriendRemrkName}","${spaceLikerRemarkName}","${content}","${secondCommentIndex}","${commentedUserId}",
+    "${commentedUserNickname}","${commentedUserAvatar}","${commentedUserFriendRemarkName}","${spaceCommentedUserRemarkName}","${commenterId}","${commenterNickname}","${commenterAvatar}","${commenterFriendRemarkName}","${spaceCommenterRemarkName}","${commentType}","${reposterId}",
+    "${reposterNickname}","${reposterAvatar}","${reposterFriendRemarkName}","${spaceUserRemarkName}","${userMomentContent}","${userMomentPhoto}","${spaceMomentContent}","${spaceMomentPhoto}","${noticeType}","${createTime}","${belongToId}")
 	`);
 }
 
